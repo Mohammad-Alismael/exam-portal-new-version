0aabc3fa50baccb903d70058c6982b8633c8a548
mhd <mhdhd@gmail.com>
2021-12-14 23:53:38 +0400

    ExamListComponent

---
+++ exam-portal/src/components/ExamListComponent.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class ExamListComponent extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+ExamListComponent.propTypes = {};
+
+export default ExamListComponent;
