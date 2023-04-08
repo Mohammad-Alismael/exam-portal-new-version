f047660775041cae14d17a311d1eadca73197dba
Mohammad-Alismael <mhdhd260@gmail.com>
2023-04-08 21:33:52 +0400

    finishing with edit classroom details

--- cypress/e2e/editCourseDetails.cy.js
+++ cypress/e2e/editCourseDetails.cy.js
@@ -1,24 +1,15 @@
 import { CoursesSelectors } from "../support/CoursesSelectors";
 import { EditCourseSelectors } from "../support/EditCourseSelectors";
+import { loginWithUserData } from "../plugins/loginPlugin";
 
 describe("Create New Course", () => {
   let coursesData;
   before(() => {
-    cy.viewport(1280, 800);
-    cy.visit("/");
+    cy.visit("/logout");
     cy.fixture("coursesData").then((data) => {
       coursesData = data;
     });
-    cy.fixture("userData").then((data) => {
-      cy.get('input[name="username"]').type(
-        data["instructor_role"]["username"]
-      );
-      cy.get('input[name="password"]').type(
-        data["instructor_role"]["password"]
-      );
-    });
-
-    cy.get('button[type="submit"]').click();
+    loginWithUserData();
     cy.url().should("include", "/courses");
   });
 
@@ -29,16 +20,16 @@ describe("Create New Course", () => {
     cy.reload();
   });
   it("edit classroom info", function () {
-    cy.wait(2000)
+    cy.wait(3000)
     EditCourseSelectors.getClassroomCard().should("exist").click();
     EditCourseSelectors.getAnnouncementComp().should("exist");
     EditCourseSelectors.getEditBtn().should("exist").click();
-    CoursesSelectors.getClassNameTextField().clear().type(
-      coursesData["courses"][3]["editCourseData"]["class_name"]
-    );
-    CoursesSelectors.getSectionTextField().clear().type(
-      coursesData["courses"][3]["editCourseData"]["section"]
-    );
+    CoursesSelectors.getClassNameTextField()
+      .clear()
+      .type(coursesData["courses"][3]["editCourseData"]["class_name"]);
+    CoursesSelectors.getSectionTextField()
+      .clear()
+      .type(coursesData["courses"][3]["editCourseData"]["section"]);
     CoursesSelectors.submitBtn().click();
     EditCourseSelectors.getCourseName()
       .should("exist")
@@ -48,18 +39,17 @@ describe("Create New Course", () => {
       );
     EditCourseSelectors.getCourseSection()
       .should("exist")
-        .should('have.prop', 'tagName', 'H3')
-      // .should(
-      //   "contain",
-      //   coursesData["courses"][3]["editCourseData"]["section"]
-      // );
+      .should("have.prop", "tagName", "H3")
+    .should(
+      "contain",
+      `section ${coursesData["courses"][3]["editCourseData"]["section"].toUpperCase()}`
+    );
   });
 
-  it('update classroom background img', function () {
-    cy.wait(2000)
+  it("update classroom background img", function () {
     EditCourseSelectors.getEditBtn().should("exist").click();
-    CoursesSelectors.getChooseBackgroundBtn().click()
-    cy.get(':nth-child(7) > img').click();
+    CoursesSelectors.getChooseBackgroundBtn().click();
+    cy.get(":nth-child(7) > img").click();
     CoursesSelectors.submitBtn().click();
   });
 });
